784ffae48696213be8cba8cec96aec0b2fd8ed67
Phuong <nphuong710207@gmail.com>
2025-10-03 09:10:02 +0700

    Phuong

---
+++ FE-DATN/vite-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+import Sidebar from './components/Sidebar.vue'
+import Register from './components/Register.vue'
+
+const showRegister = ref(false)
+</script>
+
+<template>
+  <Sidebar 
+    @open-register="showRegister = true"
+  />
+
+  <!-- Register Modal -->
+  <Register v-if="showRegister" @close="showRegister = false" />
+</template>
